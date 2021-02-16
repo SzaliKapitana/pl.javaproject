fe4fd744bc1a3e8f4022b724f870ecc0c9d75403
SzaliKapitana <rafal.szala03@gmail.com>
2021-02-16 17:20:56 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        /*System.out.println("Podaj swoje imie:");
+        Scanner scanner = new Scanner(System.in); 
+        String firstname = scanner.nextLine(); 
+        System.out.println("Twoje imie to: " + firstname);
+        scanner.close();*/
+
+        /*Scanner scanner = new Scanner(System.in); 
+        System.out.println("Podaj pierwszą liczbę:");
+        int a= scanner.nextInt();
+        System.out.println("Podaj drugą liczbe liczbę:");
+         int b= scanner.nextInt();
+        System.out.println("Podaj trzecią liczbę:");
+        int c= scanner.nextInt();    
+        System.out.println("twój wynik to: " + (a+b+c));
+        scanner.close();*/
+
+        /*if(nr == 2){
+            System.out.println("Liczba to 2");
+          }else if(nr == 3){
+            System.out.println("Liczba to 3");
+          }else{
+            System.out.println("Jakaś inna liczba niż 3 i 2");
+          }*/
+
+    }
+}
