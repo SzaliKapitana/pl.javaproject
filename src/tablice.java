ea0374737a75cbf3bbb4447ec0a9733bb4e7707d
SzaliKapitana <rafal.szala03@gmail.com>
2021-02-17 17:42:20 +0100

    v cc

--- src/tablice.java
+++ src/tablice.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
 public class tablice {
     public static void main(String[] args) throws Exception{
         
+        Scanner scanner = new Scanner(System.in);
+
+        String[] liczby = {"zero","jeden","dwa","trzy"};
+        System.out.println(liczby[2]);
+        
+        scanner.close();
+
+
     }
     
 }
