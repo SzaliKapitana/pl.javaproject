38afb19a05cd003b35d62f8d5f27b14551913d04
SzaliKapitana <rafal.szala03@gmail.com>
2021-02-17 16:21:27 +0100

    switch

--- src/Cal.java
+++ src/Cal.java
@@ -2,24 +2,44 @@ import java.util.Scanner;
 public class Cal {
     public static void main(String[] args) throws Exception{
 
+
+
+        // Dane użytkownika
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Podaj pierwszą liczbę: ");
         int a = scanner.nextInt();
 
-        System.out.println("Podaj operator + lub -: ");
+        System.out.println("Podaj operator + , - , * lub /: ");
         scanner.nextLine();
         String operator = scanner.nextLine();
 
         System.out.println("Podaj drugą liczbę: ");
         int b = scanner.nextInt();
 
-        if (operator.equals("-")){
-            System.out.println("Wynik to: " +(a-b));
-        }else if(operator.equals("+")){
+        //Switch
+        switch(operator){
+            
+            case "+":
             System.out.println("Wynik to: " +(a+b));
-        }else{
-            System.out.println("Wpisz odpowiedni operator. Spróbój ponownie.");
+            break;
+
+            case "-":
+            System.out.println("Wynik to: " +(a-b));
+            break;
+
+            case "*":
+            System.out.println("Wynik to: " +(a*b));
+            break;
+
+            case "/":
+            System.out.println("Wynik to "+(a/b)+" i reszty "+(a%b));
+            break;
+
+            default:
+            System.out.println("Podałeś zły znak");
+            break;
+
         }
             
         scanner.close();
