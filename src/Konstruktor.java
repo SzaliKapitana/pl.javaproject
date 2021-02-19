a806af5e6b2224648c97896b96e15c6891a7c2db
SzaliKapitana <rafal.szala03@gmail.com>
2021-02-19 17:02:23 +0100

    djhf

--- src/Konstruktor.java
+++ src/Konstruktor.java
@@ -1,6 +1,4 @@
-import java.util.jar.Attributes.Name;
-
-public class Konstruktor {
+public class Konstruktor{
     public static void main(String[] args) throws Exception{
 
         KonstruktorV2 KonObj = new KonstruktorV2();
